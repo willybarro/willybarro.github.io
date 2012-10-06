3d7cb004fb245da32fde87e1070e0086a594c9cc
Willy Barro <wbarro@rocket-internet.com>
2012-10-06 09:25:24 -0300

    Commit octopress source

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/willybarro/"
+http_images_path = "/willybarro/images"
+http_fonts_path = "/willybarro/fonts"
+css_dir = "public/willybarro/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
